3501236bf3e38896af5781f54147b5bb21289f8c
Marina Darievych <marindio111@gmail.com>
2024-10-23 23:41:22 -1200

    commit 241024 change

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Выберите способ отправки: 1 - DHL; 2 - Email; 3 - Owl");
+        int  choice = scanner.nextInt();
+        MailDeliveryService deliveryService = null;
+        switch (choice){
+            case 1:
+                deliveryService = new DHL();
+                break;
+            case 2:
+                deliveryService = new Email();
+                break;
+            case 3:
+                deliveryService = new Owl();
+                break;
+            default:
+                System.out.println("Неверный выбор");
+                return;
+        }
+        Sender sender = new Sender();
+        sender.send(deliveryService);
     }
 }
---
+++ src/Sender.java
@@ -0,0 +1,7 @@
+public class Sender {
+    public void send(MailDeliveryService  deliveryService) {
+        DHL service;
+        service.sendMail();
+
+    }
+}
